1afc23c0423c529c069318b903acea6b6317451e
SolomonUg <solomonchris700@gmail.com>
2025-02-17 11:58:04 +0100

    separated cart functions into its own file

--- data/cart.js
+++ data/cart.js
@@ -1,2 +1,28 @@
-const cart = [];
+export const cart = [];
 
+ export function addToCart(productId, qty){
+    let matchingItem;
+  
+      cart.forEach((cartItem) => {
+        if (cartItem.productId === productId) {
+          matchingItem =cartItem;
+        }
+      });
+      if (matchingItem) {
+        matchingItem.quantity += Number(qty);
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: Number(qty),
+        });
+      }
+  }
+
+  export function updateCartQty() {
+    let cartQuantity = 0;
+  
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  }
