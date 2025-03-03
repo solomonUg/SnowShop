b68e562f6b2abe55603b7d6baf25d7014229b066
SolomonUg <solomonchris700@gmail.com>
2025-03-03 09:58:59 +0100

    directed index.html links to root '/'

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -8,7 +8,7 @@ export function renderCheckoutHeader() {
     const checkoutHeaderHTML = `
     <div class="header-content">
         <div class="checkout-header-left-section">
-          <a href="index.html">
+          <a href="/">
             <img class="amazon-logo" src="images/snowmartlog2.jpg">
             <img class="amazon-mobile-logo" src="images/snowmart-mobile.png">
           </a>
